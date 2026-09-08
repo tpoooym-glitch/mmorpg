9186e5cbe951358b21f1f45362f0c137327ccec4
tpoooym-glitch <tpoooym@gmail.com>
2026-09-08 17:26:42 +0700

    Add centralized game configuration

---
+++ mmo/client/js/core/game-config.js
@@ -0,0 +1,8 @@
+export const GAME_CONFIG={
+  maxQuests:5,
+  world:{defaultZone:'emerald-vales'},
+  player:{baseSpeed:180,interactionRange:72},
+  combat:{attackRange:78,defaultDamage:20},
+  network:{reconnectAttempts:5,interpolation:0.15},
+  assets:{allowMissing:true}
+};
